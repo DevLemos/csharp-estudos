5b099f589aa3dcb320a5c37eb81ea19d9b4b7a72
DevLemos <kauancode@outlook.com>
2025-08-11 10:33:20 -0300

    :books: docs: Atualização das anotações Stack e Heap

---
+++ fundamentos/stack-heap/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stack_heap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack();
+            Heap();
+        }
+
+        //Stack
+        public static void Stack()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Console.WriteLine("Stack iniciando... \n");
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine($"- {stack.Pop()}");
+            }
+            Console.WriteLine("\nStack finalizada...\n");
+        }
+
+        //Heap
+        class Pessoa
+        {
+            public string Nome { get; set; }
+        }
+
+        public static void Heap()
+        {
+            Console.WriteLine("Heap iniciando... ");
+
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Michael";
+
+            Console.WriteLine($"Nome Pessoa: {p1.Nome}\n");
+
+            Pessoa p2 = p1;
+            p2.Nome = "João";
+
+            Console.WriteLine("Heap finalizada...");
+            Console.WriteLine($"Nome Pessoa: {p1.Nome}\n");
+        } 
+    }
+}
+
