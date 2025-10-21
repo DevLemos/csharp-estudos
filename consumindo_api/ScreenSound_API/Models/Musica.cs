c841b7726afe646ecddb28f2426dabb0b5e4c463
Kauan Lemos <kauancode@outlook.com>
2025-10-21 20:43:06 -0300

    :sparkles: feat: Desserialização, Serialização em JSON, LINQ, Objeto anônimo, StreamWriter

--- consumindo_api/ScreenSound_API/Models/Musica.cs
+++ consumindo_api/ScreenSound_API/Models/Musica.cs
@@ -4,11 +4,13 @@ namespace ScreenSound_API.Models;
 
 internal class Musica
 {
+    private string[] tonalidades = { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
+
     [JsonPropertyName("song")]
     public string? Nome { get; set; }
 
     [JsonPropertyName("artist")]
-    public string? Artista  { get; set; }
+    public string? Artista { get; set; }
 
     [JsonPropertyName("duration_ms")]
     public int? Duracao { get; set; }
@@ -16,11 +18,24 @@ internal class Musica
     [JsonPropertyName("genre")]
     public string? Genero { get; set; }
 
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+
+    public string Tonalidade
+    {
+
+        get
+        {
+            return tonalidades[Key];
+        }
+    }
+
     public void ExibirDetalhesMusica()
     {
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Música: {Nome}");
         Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
         Console.WriteLine($"Gênero Musical: {Genero}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
     }
 }
---
+++ consumindo_api/ScreenSound_API/Models/MusicasPreferidas.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json;
+
+namespace ScreenSound_API.Models;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+
+    public List<Musica>? MusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        MusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        MusicasFavoritas!.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as músicas favoritas -> {Nome}");
+
+        foreach(var musica in MusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+            Console.WriteLine();
+        }
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new 
+        { 
+            nome = Nome ,
+            musicas = MusicasFavoritas        
+        });
+
+        string nomeArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeArquivo, json); //escreva todo o texto
+        Console.WriteLine($"O arquivo JSON foi criado com sucesso! {Path.GetFullPath(nomeArquivo)}");
+    }
+
+    public void GerarArquivoTxt()
+    {
+        string nomeArquivo = $"musicas-favoritas-{Nome}.txt";
+
+        using (StreamWriter arquivo = new StreamWriter(nomeArquivo)) {
+
+            arquivo.WriteLine($"Músicas favoritas do {Nome}\n");
+
+            foreach (var musica in MusicasFavoritas!)
+            {
+                arquivo.WriteLine($"- {musica.Nome}");
+            }
+        }
+
+        Console.WriteLine($"txt gerado com sucesso! {Path.GetFullPath(nomeArquivo)}");
+    }
+}
