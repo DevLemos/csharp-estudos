488f9330d3999a018931f5bdd7e38f385c4d4670
DevLemos <kauancode@outlook.com>
2026-01-19 10:12:01 -0300

    :sparkles: feat: Iniciando novo projeto FIlmesAPI

---
+++ projetos/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> listaFilmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        listaFilmes.Add(filme);
+        Console.WriteLine(filme.Duracao);
+        Console.WriteLine(filme.Nome);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes()
+    {
+        return listaFilmes;
+    }
+}
---
+++ projetos/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O nome do filme é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
