f050d1eda12111e149bae3c05bf52bab391d0349
DevLemos <kauancode@outlook.com>
2026-02-03 21:57:41 -0300

    :sparkles: feat: Introdução em arquivos com bytes

---
+++ arquivos/ByteBankIO/ByteBankIO/Models/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ByteBankIO.Models
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public ContaCorrente(int numero, int agencia)
+        {
+            Numero = numero;
+            Agencia = agencia;
+            Saldo = 0;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("O valor do depósito deve ser maior que zero.", nameof(valor));
+            }
+
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("O valor do saque deve ser maior que zero.", nameof(valor));
+            }
+
+            if (valor > Saldo)
+            {
+                throw new InvalidOperationException("Saldo insuficiente para o saque.");
+
+            }
+
+            Saldo -= valor;
+        }
+    }
+}
---
+++ arquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LeituraArquivo();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        Console.WriteLine("\nPercorrendo...");
+        foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+
+        Console.WriteLine("\nFim! Esses foram os bytes preenchidos no buffer...");
+    }
+
+    static void EscreverArquivoUTF(byte[] buffer)
+    {
+        var ut8 = new UTF8Encoding();
+
+        Console.WriteLine("\nTransformando e armazenando em UTF-8...");
+        string texto = ut8.GetString(buffer);
+        Console.Write(texto);
+        Console.WriteLine("\nFinalizado!");
+    }
+
+    static void LeituraArquivo()
+    {
+        int bytesLidos = -1;
+        var buffer = new byte[1024]; //1 KB
+        int totalBytesLidos = 0;
+
+        var streamFile = new FileStream("Contas.txt", FileMode.Open);
+
+        Console.WriteLine("Começando a preencher o buffer com o fluxo de dados do arquivo...\n");
+        while((bytesLidos = streamFile.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            totalBytesLidos += bytesLidos;
+            Console.WriteLine($"Bytes lidos: {bytesLidos}, Total bytes lidos: {totalBytesLidos}");
+        }
+        Console.WriteLine("\nFim! O fluxo de dados do arquivo acabou.\n");
+
+        Console.WriteLine("Começando a percorrer o buffer preenchido com os bytes...");
+        EscreverBuffer(buffer);
+
+        Console.WriteLine("\nComeçando a percorrer o buffer preenchido com os bytes,transformados e armazenados em UTF-8...");
+        EscreverArquivoUTF(buffer);
+    }
+}
