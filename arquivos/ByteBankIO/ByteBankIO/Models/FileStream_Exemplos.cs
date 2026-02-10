a3a3b1bfd30d04adcbcd46e033ba401b2cd77fc7
DevLemos <kauancode@outlook.com>
2026-02-10 14:53:20 -0300

    :sparkles: feat: Organizando classe FileStream e introdução ao StreamReader

---
+++ arquivos/ByteBankIO/ByteBankIO/Models/FileStream_Exemplos.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace ByteBankIO.Models
+{
+    partial class FileStream_Exemplos
+    {
+        static void EscreverBuffer(byte[] buffer)
+        {
+            Console.WriteLine("\nPercorrendo...");
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("\nFim! Esses foram os bytes preenchidos no buffer...");
+        }
+
+        static void EscreverArquivoUTF(byte[] buffer, int bytesLidos)
+        {
+            var ut8 = new UTF8Encoding();
+
+            Console.WriteLine("\nTransformando e armazenando em UTF-8...");
+            string texto = ut8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+        }
+
+        static void LeituraArquivo()
+        {
+            int bytesLidos = -1;
+            var buffer = new byte[1024]; //1 KB
+            int totalBytesLidos = 0;
+
+            using (FileStream streamFile = new FileStream("Contas.txt", FileMode.Open))
+            {
+                Console.WriteLine("Começando a preencher o buffer com o fluxo de dados do arquivo...\n");
+                while (bytesLidos != 0)
+                {
+                    bytesLidos = streamFile.Read(buffer, 0, 1024);
+
+                    totalBytesLidos += bytesLidos;
+                    Console.WriteLine($"\nBytes lidos: {bytesLidos}");
+
+                    EscreverArquivoUTF(buffer, bytesLidos);
+                }
+                Console.WriteLine($"\nTotal de bytes lidos: {totalBytesLidos}");
+                Console.WriteLine("\nFim! O fluxo de dados do arquivo acabou.\n");
+
+                Console.WriteLine("Começando a percorrer o buffer preenchido com os bytes...");
+                EscreverBuffer(buffer);
+            }
+        }
+    }
+}
--- arquivos/ByteBankIO/ByteBankIO/Program.cs
+++ arquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -1,54 +1,34 @@
-﻿using System.Text;
-
-class Program
+﻿
+partial class Program
 {
     static void Main(string[] args)
     {
-        LeituraArquivo();
-    }
+        var enderecoArq = "contas.txt";
 
-    static void EscreverBuffer(byte[] buffer)
-    {
-        Console.WriteLine("\nPercorrendo...");
-        foreach (var meuByte in buffer)
+        using (FileStream fluxoArq = new FileStream(enderecoArq,FileMode.Open))
         {
-            Console.Write(meuByte);
-            Console.Write(" ");
+            //Instanciando um leitor de fluxo de dados do arquivo
+            var leitor = new StreamReader(fluxoArq);
+
+            Console.WriteLine("Lendo o arquivo e retornando a primeira linha... \n");
+            //Console.WriteLine(leitor.ReadLine());
+
+            Console.WriteLine("\nLendo o arquivo até o final (dependendo do arquivo, pode ser prejudicial pois lê tudo de uma única vez)...\n");
+            //Console.WriteLine(leitor.ReadToEnd());
+
+            Console.WriteLine("\nLendo o arquivo e retornando o primeiro byte...\n");
+            //Console.WriteLine(leitor.Read());
+
+            Console.WriteLine("\nLendo o arquivo parte por parte (linha a linha)...");
+            Console.WriteLine("Solução para quando não quiser ler o arquivo de uma única vez:\n");
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+
         }
-
-        Console.WriteLine("\nFim! Esses foram os bytes preenchidos no buffer...");
     }
 
-    static void EscreverArquivoUTF(byte[] buffer)
-    {
-        var ut8 = new UTF8Encoding();
-
-        Console.WriteLine("\nTransformando e armazenando em UTF-8...");
-        string texto = ut8.GetString(buffer);
-        Console.Write(texto);
-        Console.WriteLine("\nFinalizado!");
-    }
-
-    static void LeituraArquivo()
-    {
-        int bytesLidos = -1;
-        var buffer = new byte[1024]; //1 KB
-        int totalBytesLidos = 0;
-
-        var streamFile = new FileStream("Contas.txt", FileMode.Open);
-
-        Console.WriteLine("Começando a preencher o buffer com o fluxo de dados do arquivo...\n");
-        while((bytesLidos = streamFile.Read(buffer, 0, buffer.Length)) > 0)
-        {
-            totalBytesLidos += bytesLidos;
-            Console.WriteLine($"Bytes lidos: {bytesLidos}, Total bytes lidos: {totalBytesLidos}");
-        }
-        Console.WriteLine("\nFim! O fluxo de dados do arquivo acabou.\n");
-
-        Console.WriteLine("Começando a percorrer o buffer preenchido com os bytes...");
-        EscreverBuffer(buffer);
-
-        Console.WriteLine("\nComeçando a percorrer o buffer preenchido com os bytes,transformados e armazenados em UTF-8...");
-        EscreverArquivoUTF(buffer);
-    }
 }
